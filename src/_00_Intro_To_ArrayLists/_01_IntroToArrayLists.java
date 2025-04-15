7d57af54ccccba28beffdaa2f1ab5e0906b7cc2b
Siddharth <sidkhandelwal2010@gmail.com>
2025-04-14 19:58:18 -0700

    run guest book to find error, fix show names button in actionPerfromed

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -1,21 +1,42 @@
 package _00_Intro_To_ArrayLists;
 
+import javax.swing.JOptionPane;
+
 public class _01_IntroToArrayLists {
     public static void main(String[] args) {
         // 1. Create an array list of Strings
         //    Don't forget to import the ArrayList class
-
+String[] strings = new String[5];
         // 2. Add five Strings to your list
-
+strings[0] = "bob";
+strings[1] = "jeff";
+strings[2] = "tom";
+strings[3] = "jerry";
+strings[4] = "tim";
         // 3. Print all the Strings using a standard for-loop
-
+for (int i = 0; i < strings.length; i++) {
+	JOptionPane.showMessageDialog(null, strings[i]);
+}
         // 4. Print all the Strings using a for-each loop
-
+for (String string : strings) {
+	JOptionPane.showMessageDialog(null, string);
+}
         // 5. Print only the even numbered elements in the list.
-
+for (int i = 0; i < strings.length; i+=2) {
+	JOptionPane.showMessageDialog(null, strings[i]);
+}
         // 6. Print all the Strings in reverse order.
-
+for (int i = strings.length-1; i >= 0; i--) {
+	JOptionPane.showMessageDialog(null, strings[i]);
+}
         // 7. Print only the Strings that have the letter 'e' in them.
-        
+        for (int i = 0; i < strings.length; i++) {
+        	for (int j = 0; j < strings[i].length(); j++) {
+				if(strings[i].charAt(j) == 'e') {
+				JOptionPane.showMessageDialog(null, strings[i]);
+			}
+			
+			}
+		}
     }
 }
--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,17 +1,62 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
-    /*
-     * Create a GUI with two buttons. One button reads "Add Name" and the other
-     * button reads "View Names". When the add name button is clicked, display
-     * an input dialog that asks the user to enter a name. Add that name to an
-     * ArrayList. When the "View Names" button is clicked, display a message
-     * dialog that displays all the names added to the list. Format the list as
-     * follows:
-     * Guest #1: Bob Banders
-     * Guest #2: Sandy Summers
-     * Guest #3: Greg Ganders
-     * Guest #4: Donny Doners
-     */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener{
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton addName = new JButton("Add Name");
+		JButton show = new JButton("View Names");
+		int numOfNames = 0;
+		String[] listOfNames = new String[numOfNames];
+		/*
+		 * Create a GUI with two buttons. One button reads "Add Name" and the other
+		 * button reads "View Names". When the add name button is clicked, display
+		 * an input dialog that asks the user to enter a name. Add that name to an
+		 * ArrayList. When the "View Names" button is clicked, display a message
+		 * dialog that displays all the names added to the list. Format the list as
+		 * follows:		
+		 * Guest #1: Bob Banders
+		 * Guest #2: Sandy Summers
+		 * Guest #3: Greg Ganders
+		 * Guest #4: Donny Doners
+		 */
+		public void run() {
+		// TODO Auto-generated method stub
+		
+		
+		frame.add(panel);
+		panel.add(addName);
+		panel.add(show);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		addName.addActionListener(this);
+		show.addActionListener(this);
+	}
+
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// This code will be executed when the button is clicked
+JButton buttonClicked = (JButton)e.getSource();
+if(buttonClicked==addName) {
+	 String name = JOptionPane.showInputDialog("Please enter a name to add to the Guest Book");
+	 numOfNames++;
+	 listOfNames[numOfNames] = name;
+}
+if(buttonClicked==show) {
+	for (int i = 0; i < listOfNames.length; i++) {
+		JOptionPane.showMessageDialog(null,"Guest #"+numOfNames+" "+listOfNames[i]);
+	}
+}
+		
+	}
+	
 }
