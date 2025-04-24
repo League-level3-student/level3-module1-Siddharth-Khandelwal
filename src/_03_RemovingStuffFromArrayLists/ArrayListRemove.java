ebd30ffc235701aaf769d088bd0f2bbc801345e4
Siddharth <sidkhandelwal2010@gmail.com>
2025-04-23 19:57:02 -0700

    Finished ArrayListRemove, got to step 2 of card game

--- src/_03_RemovingStuffFromArrayLists/ArrayListRemove.java
+++ src/_03_RemovingStuffFromArrayLists/ArrayListRemove.java
@@ -4,44 +4,47 @@ import java.util.ArrayList;
 
 public class ArrayListRemove {
 
-    class Stuff {
-        public String type;
-    }
-    
-    class Worm extends Stuff {
-        public Worm() {
-            type = "worm";
-        }
-    }
-    
-    class Dirt extends Stuff {
-        public Dirt() {
-            type = "dirt";
-        }
-    }
+	class Stuff {
+		public String type;
+	}
 
-    // 1. Write a method that removes the dirt in the yard and returns the
-    //    ArrayList
-    public static ArrayList<Stuff> cleanOutTheYard( ArrayList<Stuff> yard ) {
-        for (int i = 0; i < yard.size(); i++) {
-		if(yard.get(i).type.equals("dirt")) {
-			yard.remove(i);
+	class Worm extends Stuff {
+		public Worm() {
+			type = "worm";
 		}
-        } 
+	}
 
-        return yard;
-        
-    }
-    
-    // 2. Write a method that removes the hash tag ('#') characters from the
-    //    ArrayList and returns it
-    public static ArrayList<Character> removeHashTags(ArrayList<Character> list) {
-    	for (int i = 0; i < list.size(); i++) {
+	class Dirt extends Stuff {
+		public Dirt() {
+			type = "dirt";
+		}
+	}
 
-    	//	if(list.get(i).charValue()) {
-    			list.remove(i);
-    		}
-           // } 
-        return list;
-    }
+	// 1. Write a method that removes the dirt in the yard and returns the
+	// ArrayList
+	public static ArrayList<Stuff> cleanOutTheYard(ArrayList<Stuff> yard) {
+		String removing = "dirt";
+		for (int i = 0; i < yard.size(); i++) {
+			if (yard.get(i).type.equals(removing)) {
+				yard.remove(i);
+				i--;
+			}
+		}
+		
+		return yard;
+
+	}
+
+	// 2. Write a method that removes the hash tag ('#') characters from the
+	// ArrayList and returns it
+	public static ArrayList<Character> removeHashTags(ArrayList<Character> list) {
+		char targetChar = '#';
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).charValue() == targetChar)
+				// if(list.get(i).charValue()) {
+				list.remove(i);
+		}
+		// }
+		return list;
+	}
 }
